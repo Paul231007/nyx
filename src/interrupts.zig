8cdc7750fb156be55070f67b0de89bc3e8550ebc
Paul231007 <kornpaul2310@gmail.com>
2026-07-11 10:24:40 +0200

    rough interrupts

--- src/interrupts.zig
+++ src/interrupts.zig
@@ -60,3 +60,6 @@ const exception_names = [_][]const u8{
     "SIMD FP Exception", // 19
 };
 
+fn nameOf(n: u32) []const u8 {
+    return if (n < exception_names.len) exception_names[n] else "Reserved";
+}
