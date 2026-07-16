23d296db14500c8576026a1643a99e16f1586a7a
Paul231007 <kornpaul2310@gmail.com>
2026-07-16 13:18:00 +0200

    sketch io

--- src/io.zig
+++ src/io.zig
@@ -1,2 +1,9 @@
 //! Port-mapped I/O primitives.
 
+pub inline fn outb(port: u16, value: u8) void {
+    asm volatile ("outb %[value], %[port]"
+        :
+        : [value] "{al}" (value),
+          [port] "{dx}" (port),
+    );
+}
