b84b313bc45c127186ddec68fd8de15fd9be915f
Paul231007 <kornpaul2310@gmail.com>
2026-07-17 16:39:11 +0200

    rough pic

--- src/pic.zig
+++ src/pic.zig
@@ -2,3 +2,6 @@
 //! After remap, master IRQs 0..7 land on vectors 0x20..0x27 and slave IRQs
 //! 8..15 on 0x28..0x2F, so they no longer collide with CPU exceptions.
 
+const io = @import("io.zig");
+
+
