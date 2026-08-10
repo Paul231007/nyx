6fb1468d42db386fe52da45c1a74bb7df93e1322
Paul231007 <kornpaul2310@gmail.com>
2026-08-10 15:05:43 +0200

    start pci

--- src/pci.zig
+++ src/pci.zig
@@ -2,3 +2,14 @@
 
 const io = @import("io.zig");
 
+/// A discovered PCI device.
+pub const Device = struct {
+    bus: u8,
+    slot: u8,
+    func: u8,
+    vendor: u16,
+    device: u16,
+    class: u8,
+    subclass: u8,
+};
+
