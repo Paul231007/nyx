0de5c7253b853d7c7104b9711d81b19d4ce173a6
Paul231007 <kornpaul2310@gmail.com>
2026-08-12 16:39:39 +0200

    formatting

--- src/pci.zig
+++ src/pci.zig
@@ -53,3 +53,4 @@ pub fn enumerate(out: []Device) usize {
                 const subclass: u8 = @truncate((class_dword >> 16) & 0xFF);
                 const class: u8 = @truncate((class_dword >> 24) & 0xFF);
 
+
